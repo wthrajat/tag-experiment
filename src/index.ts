6fb58dcd65df4d9c9e8c860f30b6ddcd2e11339a
wthrajat <rajatkhanduri290102@gmail.com>
2025-06-20 02:31:44 +0530

    feat: pvt repo proof

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
+const TOKEN = process.env.GITHUB_TOKEN;
+
 export const getLatestTag = async (): Promise<string> => {
   try {
-    const response = await fetch('https://api.github.com/repos/wthrajat/tag-experiment/releases/latest');
+    const response = await fetch(
+      'https://api.github.com/repos/wthrajat/tag-experiment/releases/latest',
+      {
+        headers: {
+          'Authorization': `Bearer ${TOKEN}`,
+          'Accept': 'application/vnd.github+json',
+          'X-GitHub-Api-Version': '2022-11-28',
+        }
+      }
+    );
+
     const data = await response.json();
     const rawTag = data.tag_name;
     const cleanTag = rawTag.startsWith('v') ? rawTag.slice(1) : rawTag;
