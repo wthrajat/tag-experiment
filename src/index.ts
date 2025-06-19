cc8735b9eee7e846d21fa9431a3d1e2f064a5918
wthrajat <rajatkhanduri290102@gmail.com>
2025-06-20 01:25:30 +0530

    chore: nit

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ export const getLatestTag = async (): Promise<string> => {
   try {
     const response = await fetch('https://api.github.com/repos/wthrajat/tag-experiment/releases/latest');
     const data = await response.json();
-    return data.tag_name;
-  } catch (error) {
-    console.error('Failed to fetch latest tag, falling back to default');
-    return '0'
+    const rawTag = data.tag_name;
+    const cleanTag = rawTag.startsWith('v') ? rawTag.slice(1) : rawTag;
+    return cleanTag;
+  } catch (e) {
+    console.error(e)
+    throw new Error('Failed to fetch latest tag!');
   }
 };
 
