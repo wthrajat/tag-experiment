b0f48a255d41d48a4cb48cb1d89ecb1736ff966b
wthrajat <rajatkhanduri290102@gmail.com>
2025-07-10 19:32:56 +0530

    chore: workflows

---
+++ src/tags.test.ts
@@ -0,0 +1,9 @@
+// import test from "node:test";
+
+// import { getLatestSemverInMajor } from "./getLatestSemverInMajor";
+
+// test("gets latest 0.x.x tag correctly", async () => {
+//   const tags = ["v0.6.7", "v1.2.4", "v0.0.9", "v0.5.1"];
+//   const result = await getLatestSemverInMajor(tags, 0);
+//   expect(result).toBe("0.6.7");
+// });
